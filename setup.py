0f3bf885b30ddf4afae7eba0f8d6ccb704bc6c87
Bruno Farias <brunocarvalhofarias@gmail.com>
2024-12-16 19:33:42 -0400

    Add project description

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="ethcheck",
-    version="0.1.0",
+    version="0.1.1",
     description="A Python tool for verifying Ethereum Consensus Specification using ESBMC",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author="Bruno Farias",
     author_email="brunocarvalhofarias@gmail.com",
     url="https://github.com/esbmc/ethcheck",
